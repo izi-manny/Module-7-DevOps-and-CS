818e785d67a8afaec4e74c8d4f22daa029248d45
Emmanuel Izilov <em5750@gmail.com>
2021-10-29 18:36:05 -0400

    changed equality

--- cs/uniqueChars.js
+++ cs/uniqueChars.js
@@ -4,7 +4,7 @@ function hasUniqueChars(str){
     // If there is a duplicate of a character, return false
     for(let i = 0; i < str.length; i++)
         for(let j = i + 1; j < str.length; j++)
-            if (str[i] == str[j])
+            if (str[i] === str[j])
                 return false;
  
     // If no duplicate characters found, return true

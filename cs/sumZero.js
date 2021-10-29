a7d9951ef404800bbab64ca6c503bb432912f340
Emmanuel Izilov <em5750@gmail.com>
2021-10-29 18:37:11 -0400

    function modified

--- cs/sumZero.js
+++ cs/sumZero.js
@@ -1,5 +1,21 @@
 // a function that takes in an array of numbers. The function should return True if any two numberss in list sum to 0, and false otherwise.
 
-function addToZero(numArr){
-    
+// Jaxon's solution
+
+function sumZero(arr) {
+
+    if (arr.length === 0 || arr.length === 1) {
+        return 'false';
+    }
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        // Start at i+1 to prevent from comparing first item with itself
+        for (let j = i + 1; j < arr.length;j++) {
+            if (arr[i] + arr[j] === 0) {
+                return 'true';
+            }
+        }
+    }
+
+    return 'false';
 }

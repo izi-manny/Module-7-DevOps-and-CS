94e8630e80370d917cf9b1ee7be947210ce7440f
Emmanuel Izilov <em5750@gmail.com>
2021-10-29 14:15:39 -0400

    initial commit

---
+++ cs/sumZero.js
@@ -0,0 +1,5 @@
+// a function that takes in an array of numbers. The function should return True if any two numberss in list sum to 0, and false otherwise.
+
+function addToZero(numArr){
+    
+}
---
+++ cs/uniqueChars.js
@@ -0,0 +1,20 @@
+// a function that takes in a single word, as a string. It should return True if that word contains only unique characters. Return False otherwise.
+
+function hasUniqueChars(str){
+    // If there is a duplicate of a character, return false
+    for(let i = 0; i < str.length; i++)
+        for(let j = i + 1; j < str.length; j++)
+            if (str[i] == str[j])
+                return false;
+ 
+    // If no duplicate characters found, return true
+    return true;
+}
+
+hasUniqueChars("Monday");
+// -> True
+
+hasUniqueChars("Moonday");
+// -> False
+
+// Function time complexity: O(n^2)
